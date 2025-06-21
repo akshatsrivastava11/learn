c7bf34f9598a46c17dd60ab8a127533a326f3bd4
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-06-21 15:48:19 +0530

    2hooksCreated+NavbarAdded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,35 @@
-'use client'
-import { extractPdf } from "@/hooks/extractPdf";
-import { useState } from "react";
-// import { extractPDFText } from ""; // path may vary
+import GooeyNav from "@/blocks/Components/GooeyNav/GooeyNav";
+import Image from "next/image";
 
 export default function Home() {
-  const [file, setFile] = useState<File | null>(null);
-  const [pdfText, setPdfText] = useState("");
-  const [progress, setProgress] = useState(0);
+  const items = [
 
-  const handleSubmit = async () => {
-    if (!file) return;
-    const text = await extractPdf(file);
-    setPdfText(text);
-  };
+  { label: "Home", href: "#" },
 
+  { label: "About", href: "#" },
+
+  { label: "Contact", href: "#" },
+
+];
   return (
-    <>
-      <h1>PDF OCR</h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={(e) => {
-            const files = e.target.files;
-            if (files && files[0]) setFile(files[0]);
-          }}
-        />
-        <button type="submit">Submit</button>
-      </form>
+<GooeyNav
 
-      <div>
-        <progress value={progress} max={1} />
-        <pre>{pdfText}</pre>
-      </div>
-    </>
+    items={items}
+
+    particleCount={15}
+
+    particleDistances={[90, 10]}
+
+    particleR={100}
+
+    initialActiveIndex={0}
+
+    animationTime={600}
+
+    timeVariance={300}
+
+    colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+
+  />
   );
 }
