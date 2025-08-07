652f73819ac94613cb491da9222d4fec050d54ea
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-08-07 23:38:51 +0530

    FinalWorkingCommit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig:NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env:{
+    TOGETHER_API_KEY: process.env.TOGETHER_API_KEY
+  }
 }
 
 module.exports = nextConfig
--- src/hooks/useLLM.tsx
+++ src/hooks/useLLM.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { headers } from 'next/headers'
-const Together_api_key="156d88350d5bef6d35c983189f32dbd5e426fa6263efeb7ad22025922ee1306d"
+import dotenv from 'dotenv'
+dotenv.config()
 const meta_llama_endpoint="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
 const together_api="https://api.together.xyz/v1/chat/completions"
 // JSON.stringify
@@ -46,7 +47,7 @@ export const useLLM=async(message:string,usage:string)=>{
         ,{
             headers:{
                 'Content-Type':'application/json',
-                'Authorization':`Bearer ${Together_api_key}`
+                'Authorization':`Bearer ${process.env.TOGETHER_API_KEY}`
             }
 
         }
